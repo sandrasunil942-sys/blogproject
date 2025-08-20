2411e3b593210da743255685ae506329835e04f3
sandrasunil942-sys <sandrasunil942@gmail.com>
2025-08-20 10:30:38 +0530

    Delete blog_project directory

--- blog_project/blogapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class BlogappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blogapp'
-
-    def ready(self):
-        import blogapp.signals
